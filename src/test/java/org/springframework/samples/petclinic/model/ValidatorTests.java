68c9d0902aa4f532132f2ec02b59bd817846ecad
alexandreponte2 <alexandre.s.ponte@concrete.com.br>
2021-03-09 22:10:40 -0300

    remove goku

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("GOKU");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
